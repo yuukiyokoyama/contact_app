a75bafb824c28a4aad3f7a2d816b4d0db1d398d7
yuukiyokoyama <yuukitigars89@gmail.com>
2020-09-15 12:36:37 +0900

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -56,8 +56,10 @@ class User < ApplicationRecord
  
   # ユーザーのステータスフィードを返す
   def feed
-    part_of_feed = "relationships.follower_id = :id or microposts.user_id = :id"
-    Micropost.joins(user: :followers).where(part_of_feed, { id: id })
+    following_ids = "SELECT followed_id FROM relationships
+                     WHERE follower_id = :user_id"
+    Micropost.where("user_id IN (#{following_ids})
+                     OR user_id = :user_id", user_id: id)
   end
 
     # ユーザーをフォローする
