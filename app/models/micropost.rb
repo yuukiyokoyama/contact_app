c46406d7f18454b7fbbf11e97a4d0bebaebcd0cb
yuukiyokoyama <yuukitigars89@gmail.com>
2020-10-31 21:05:15 +0900

    返信機能2

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -30,6 +30,13 @@ mapping: %w(content micropost_content)
 
 before_validation :assign_in_reply_to
 
+before_validation :set_in_reply_to # ここ
+
+validates :user_id, presence: true
+validates :content, presence: true, length: {maximum: 140}
+validates :in_reply_to, presence: false
+validate :picture_size, :reply_to_user # ここ
+
 private
 
     def assign_in_reply_to
@@ -54,4 +61,17 @@ private
 
   def Micropost.including_replies(id)
     where(in_reply_to: [id, 0]).or(Micropost.where(user_id: id))
+end
+
+def set_in_reply_to
+  if @index = content.index("@")
+    reply_id = []
+    while is_i?(content[@index+1])
+      @index += 1
+      reply_id << content[@index]
+    end
+    self.in_reply_to = reply_id.join.to_i
+  else
+    self.in_reply_to = 0
+  end
 end
