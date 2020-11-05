39ec6f85cc966aea94708d76128376bff52a0a06
yuukiyokoyama <yuukitigars89@gmail.com>
2020-11-05 23:00:17 +0900

    アカウントの有効化1

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ApplicationRecord
   class UserTest < ActiveSupport::TestCase
+    attr_accessor :remember_token, :activation_token
+    before_save   :downcase_email
+    before_create :create_activation_digest
   has_many :likes, dependent: :destroy
   has_many :memberships, dependent: :destroy
   has_meny :class_name: "Talk", through: :memberships
@@ -129,3 +132,14 @@ class User < ApplicationRecord
     end
   end
 
+    # メールアドレスをすべて小文字にする
+    def downcase_email
+      self.email = email.downcase
+    end
+
+    # 有効化トークンとダイジェストを作成および代入する
+    def create_activation_digest
+      self.activation_token  = User.new_token
+      self.activation_digest = User.digest(activation_token)
+    end
+end
