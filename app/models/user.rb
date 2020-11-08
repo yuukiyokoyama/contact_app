1e25bb14b41a7962a22f5953cce9560ea1dbf4c9
yuukiyokoyama <yuukitigars89@gmail.com>
2020-11-08 23:16:21 +0900

    アカウントの有効化3

--- app/models/user.rb
+++ app/models/user.rb
@@ -137,9 +137,28 @@ class User < ApplicationRecord
       self.email = email.downcase
     end
 
+      # トークンがダイジェストと一致したらtrueを返す
+  def authenticated?(attribute, token)
+    digest = send("#{attribute}_digest")
+    return false if digest.nil?
+    BCrypt::Password.new(digest).is_password?(token)
+  end
+
+    # アカウントを有効にする
+    def activate
+      update_attribute(:activated,    true)
+      update_attribute(:activated_at, Time.zone.now)
+    end
+  
+    # 有効化用のメールを送信する
+    def send_activation_email
+      UserMailer.account_activation(self).deliver_now
+    end
+
     # 有効化トークンとダイジェストを作成および代入する
     def create_activation_digest
       self.activation_token  = User.new_token
       self.activation_digest = User.digest(activation_token)
     end
 end
+
