7a14475a6c67ea891921baf18427cdf9c2598db7
yuukiyokoyama <yuukitigars89@gmail.com>
2020-11-11 23:46:39 +0900

    パスワードの再設定2

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   class UserTest < ActiveSupport::TestCase
-    attr_accessor :remember_token, :activation_token
+    attr_accessor :remember_token, :activation_token, :reset_token
     before_save   :downcase_email
     before_create :create_activation_digest
   has_many :likes, dependent: :destroy
@@ -153,6 +153,18 @@ class User < ApplicationRecord
     def send_activation_email
       UserMailer.account_activation(self).deliver_now
     end
+
+  # パスワード再設定の属性を設定する
+  def create_reset_digest
+    self.reset_token = User.new_token
+    update_attribute(:reset_digest,  User.digest(reset_token))
+    update_attribute(:reset_sent_at, Time.zone.now)
+  end
+
+  # パスワード再設定のメールを送信する
+  def send_password_reset_email
+    UserMailer.password_reset(self).deliver_now
+  end
 
     # 有効化トークンとダイジェストを作成および代入する
     def create_activation_digest
